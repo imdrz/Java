2c9cdf38e9d0263b5d8fe473d0dee22e31dcb9f0
BuildTools <unconfigured@null.spigotmc.org>
2024-07-17 21:22:59 -0400

    Sieve Algorithim Started

--- Bismillah/src/Algorithims/NthPrime.java
+++ Bismillah/src/Algorithims/NthPrime.java
@@ -6,9 +6,18 @@ public class NthPrime {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int nth = 100; //541
-        int size = nth * 2;
+        for (int x = 0; x < SieveOfEratosthenes(nth).length; x++) {
+            System.out.println(SieveOfEratosthenes(nth)[x]);
+        }
     }
-    public static int SieveOfEratosthenes(int n) {
-        return 0;
+    public static int[] SieveOfEratosthenes(int n) {
+        int size = n*2;
+        int[] nums = new int[size];
+        int index = 0;
+        for (int x = 2; x < size; x++) {
+            nums[index] = x;
+            index++;
+        }
+        return nums;
     }
 }
